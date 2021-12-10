ef852c3ac1281b8e85e6ae350a6f85301d16a482
Travis F <agathangalos@gmail.com>
2021-12-10 18:01:18 -0500

    Implemented Eraser and Clear buttons. Added control to change canvas color. Added some HTML and edited CSS styles.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
+// Default variables
 let rows = 64;
 let columns = 64;
 let color = "#0000ff";
+let bgColor = '#ffffff';
 
+// Makes the grid!
 const grid = document.getElementById('grid-container');
-
 const makeGrid = (rows, columns) => {
     // set size of grid template css properties
     grid.style.setProperty('--grid-rows', rows);
     grid.style.setProperty('--grid-cols', columns);
+    grid.style.backgroundColor = bgColor;
     // generate the grid's cells
     for (i = 0; i < (rows * columns); i++) {
         let cell = document.createElement('div');
@@ -34,7 +37,20 @@ const makeGrid = (rows, columns) => {
 makeGrid(rows, columns);
 
 // Grid size selector
-const sizer = document.querySelector('input');
+const sizer = document.querySelector('#grid-size-selector');
+sizer.addEventListener('input', (e) => {
+    //console.log(e.target.valueAsNumber);
+    let p = document.querySelector('#size-display');
+    p.textContent = `${e.target.value}x${e.target.value}`;
+    rows = e.target.valueAsNumber;
+    columns = e.target.valueAsNumber;
+});
+
+const submitSize = document.querySelector('#size-submit');
+submitSize.addEventListener('click', () => {
+    clearCanvas();
+    makeGrid(rows, columns);
+});
 
 // Button event listeners
 
@@ -45,15 +61,44 @@ colorButton.addEventListener('input', (e) => {
     color = e.target.value;
 });
 
+const bgButton = document.querySelector('#background-picker');
+bgButton.value = bgColor;
+bgButton.addEventListener('input', (e) => {
+    bgTemp = bgColor;
+    bgColor = e.target.value;
+    console.log(bgTemp);
+    let cells = document.querySelectorAll('div.grid-item');
+    cells.forEach(div => {
+        if (div.style.backgroundColor != color)
+            div.style.backgroundColor = bgColor;
+    });
+});
+
+
 const eraseButton = document.getElementById('erase-button');
 eraseButton.addEventListener('click', () => {
-    color = "ffffff";
+    let p = document.querySelector('#erase-button');
+    if (p.textContent == 'Eraser: On') {
+        color = colorButton.value;
+        p.textContent = 'Eraser: Off';
+    } else if (p.textContent == 'Eraser: Off') {
+        p.textContent = 'Eraser: On';
+        color = bgColor;    
+    }
 });
 
 const resetButton = document.getElementById('reset-button');
 resetButton.addEventListener('click', () => {
+    clearCanvas();
+});
+
+
+// function to return the canvas to blank whitespace
+const clearCanvas = () => {
     let cells = document.querySelectorAll('div.grid-item');
     cells.forEach(div => {
-        div.style.backgroundColor = 'white';
+        div.style.backgroundColor = bgColor;
     });
-});
+    color = colorButton.value;
+    bgColor = bgButton.value;
+};
